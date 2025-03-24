91655e29b4698f24a91cb646cbfb0cd012226710
dedwar65 <dedwar65@jh.edu>
2025-03-24 10:49:42 -0400

    edits to slides for presentation on 3/25

---
+++ auto/Chp1-presentation.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "Chp1-presentation"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("biblatex" "backend=bibtex" "style=authoryear")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "biblatex"
+    "dirtytalk"
+    "cancel"
+    "graphicx"
+    "wrapfig"
+    "caption"
+    "booktabs"
+    "adjustbox")
+   (LaTeX-add-bibliographies
+    "Chp1"))
+ :latex)
+
