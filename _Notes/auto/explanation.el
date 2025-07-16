a79547e9884702ad9b24abdaaed242b1f9aafbdb
dedwar65 <dedwar65@jh.edu>
2025-07-16 12:34:37 -0400

    right up with concerns about paper

--- _Notes/auto/explanation.el
+++ _Notes/auto/explanation.el
@@ -1,10 +1,22 @@
 (TeX-add-style-hook
  "explanation"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("biblatex" "backend=bibtex" "style=authoryear")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art10"
-    "footnote"))
+    "footnote"
+    "babel"
+    "biblatex")
+   (TeX-add-symbols
+    '("say" 1))
+   (LaTeX-add-bibliographies
+    "Chp1"))
  :latex)
 
