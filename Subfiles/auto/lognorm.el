58564a7380a83b01872872117f14f7e1e5156005
dedwar65 <dedwar65@jh.edu>
2025-10-16 11:14:35 -0400

    files

---
+++ Subfiles/auto/lognorm.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "lognorm"
+ (lambda ()
+   (TeX-add-symbols
+    "figpath"
+    "tblpath")
+   (LaTeX-add-labels
+    "sec:lognorm"
+    "fig:PYLognorm"
+    "fig:LCLognorm"
+    "fig:LognormPYLCMPCCompare"
+    "fig:SimLorenzTarPoint"
+    "fig:SimLorenzTarDist"))
+ :latex)
+
--- auto/Chp1-draft.el
+++ auto/Chp1-draft.el
@@ -3,12 +3,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("placeins" "section")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -20,12 +20,14 @@
     "Subfiles/results"
     "Subfiles/taxation"
     "Subfiles/mechanism"
+    "Subfiles/lognorm"
     "Subfiles/conclusion"
     "article"
     "art10"
     "subfiles"
     "footnote"
     "lipsum"
+    "graphicx"
     "float"
     "placeins"
     "afterpage"
